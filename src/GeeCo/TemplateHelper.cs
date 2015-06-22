4bf9ebf0481acd43f0f170d3e40350ba907f5a57
l.laborie <l.laborie@criteo.com>
2015-06-22 18:00:27 +0200

    Use Generic Json engine

--- src/GeeCo/TemplateHelper.cs
+++ src/GeeCo/TemplateHelper.cs
@@ -1,11 +1,11 @@
 ﻿using Cottle;
 using Cottle.Documents;
-using Cottle.Functions;
 using Cottle.Settings;
 using Cottle.Stores;
+using Cottle.Values;
 using Microsoft.ClearScript;
 using Microsoft.ClearScript.V8;
-using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -86,7 +86,7 @@ namespace GeeCo
                 }
             }
 
-            store = TemplateHelper.BuildStore(json, metadata);
+            store = TemplateHelper.BuildStore(json, metadata);            
 
             if (!Directory.GetParent(outputPath).Exists)
                 Directory.GetParent(outputPath).Create();
@@ -105,11 +105,12 @@ namespace GeeCo
 
         private static IStore BuildStore(string json, params KeyValuePair<string, string>[] metadata)
         {
-            Global global = JsonConvert.DeserializeObject<Global>(json);
-
             IStore store = new BuiltinStore();
 
-            global.Fill(store);
+            JObject content = JObject.Parse(json);
+
+            foreach (JProperty property in content.Properties())
+                store[property.Name] = TemplateHelper.Convert(property.Value);
 
             foreach (var pair in metadata)
                 store[pair.Key] = pair.Value;
@@ -117,241 +118,91 @@ namespace GeeCo
             return store;
         }
 
+        private static Value Convert(JToken token)
+        {
+            if (token is JArray)
+            {
+                List<Value> array = new List<Value>();
+                foreach(var element in (JArray)token)
+                    array.Add(TemplateHelper.Convert(element));
+
+                return array;
+            }
+            if (token is JObject)
+            {
+                Dictionary<Value, Value> dico = new Dictionary<Value, Value>();
+                foreach (JProperty property in ((JObject)token).Properties())
+                    dico[property.Name] = TemplateHelper.Convert(property.Value);
+
+                return dico;
+            }
+            if (token is JValue)
+            {
+                JValue value = (JValue)token;
+                switch (value.Type)
+                {
+                    case JTokenType.Array:
+                        List<Value> array = new List<Value>();
+                        foreach(var element in (JArray)token)
+                            array.Add(TemplateHelper.Convert(element));
+
+                    return array;
+
+                    case JTokenType.Boolean:
+                        return (bool)value.Value;
+
+                    case JTokenType.Bytes:
+                        List<Value> bytes = new List<Value>();
+                        foreach (byte b in (byte[])value.Value)
+                            bytes.Add(b);
+
+                        return bytes;
+
+                    case JTokenType.Comment:
+                    case JTokenType.Property:
+                    case JTokenType.Raw:
+                    case JTokenType.String:
+                        return (string)(value.Value);
+
+                    case JTokenType.Date:
+                        return ((DateTime)(value.Value)).ToString("yyyy-MM-ddTHH:mm:ss");
+
+                    case JTokenType.Float:
+                        return (double)(value.Value);
+
+                    case JTokenType.Guid:
+                        return ((Guid)(value.Value)).ToString();
+
+                    case JTokenType.Integer:
+                        return (long)(value.Value);
+
+                    case JTokenType.None:
+                    case JTokenType.Null:
+                    case JTokenType.Undefined:
+                        return VoidValue.Instance;
+
+                    case JTokenType.Object:
+                        Dictionary<Value, Value> dico = new Dictionary<Value, Value>();
+                        foreach (JProperty property in ((JObject)token).Properties())
+                            dico[property.Name] = TemplateHelper.Convert(property.Value);
+
+                        return dico;
+
+                    case JTokenType.TimeSpan:
+                        return ((TimeSpan)(value.Value)).ToString();
+
+                    case JTokenType.Uri:
+                        return ((Uri)(value.Value)).ToString();
+                    
+                }
+            }
+
+            return null;
+        }
+
         #region Types
 
         private delegate void RunTemplateHandler(string templatePath, string outputPath, params KeyValuePair<string, string>[] metadata);
-
-        private struct Global
-        {
-            public Database database { get; set; }
-
-            public void Fill(IStore store)
-            {
-                store["database"] = this.database.Store();
-            }
-        }
-
-        private struct Database
-        {
-            public string name { get; set; }
-            public string project_guid { get; set; }
-            public string solution_guid { get; set; }
-
-            public List<Table> tables { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["name"] = this.name;
-                result["project_guid"] = this.project_guid;
-                result["solution_guid"] = this.solution_guid;
-
-                Dictionary<Value, Value> tables = new Dictionary<Value, Value>();
-                foreach (var table in this.tables)
-                {
-                    tables[table.name] = table.Store();
-                }
-                result["tables"] = tables;
-
-                return result;
-            }
-        }
-
-        private struct Table
-        {
-            public string name { get; set; }
-            public string clients { get; set; }
-            public Prevision size { get; set; }
-            public Prevision activity { get; set; }
-            public string data_scope { get; set; }
-            public bool replication { get; set; }
-            public List<Column> columns { get; set; }
-            public List<ForeignKey> foreign_keys { get; set; }
-            public List<Index> indexes { get; set; }
-            public List<ReverseForeignKeys> reverse_foreign_keys { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["name"] = this.name;
-                result["clients"] = this.clients;
-                result["size"] = this.size.Store();
-                result["activity"] = this.activity.Store();
-                result["data_scope"] = this.data_scope;
-                result["replication"] = this.replication;
-
-                Dictionary<Value, Value> columns = new Dictionary<Value, Value>();
-                foreach (var column in this.columns)
-                {
-                    columns[column.name] = column.Store();
-                }
-                result["columns"] = columns;
-
-                if (this.foreign_keys != null)
-                {
-                    Dictionary<Value, Value> foreign_keys = new Dictionary<Value, Value>();
-                    foreach (var foreign_key in this.foreign_keys)
-                    {
-                        foreign_keys[foreign_key.name] = foreign_key.Store();
-                    }
-                    result["foreign_keys"] = foreign_keys;
-                }
-
-                if (this.indexes != null)
-                {
-                    Dictionary<Value, Value> indexes = new Dictionary<Value, Value>();
-                    foreach (var index in this.indexes)
-                    {
-                        indexes[index.name] = index.Store();
-                    }
-                    result["indexes"] = indexes;
-                }
-
-                if (this.reverse_foreign_keys != null)
-                {
-                    List<Value> reverse_foreign_keys = new List<Value>();
-                    foreach (var reverse_foreign_key in this.reverse_foreign_keys)
-                    {
-                        reverse_foreign_keys.Add(reverse_foreign_key.Store());
-                    }
-                    result["reverse_foreign_keys"] = reverse_foreign_keys;
-
-                }
-                return result;
-            }
-        }
-
-        private struct Prevision
-        {
-            public string today { get; set; }
-            public string three_month { get; set; }
-            public string one_year { get; set; }
-            public string three_years { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["today"] = this.today;
-                result["three_month"] = this.three_month;
-                result["one_year"] = this.one_year;
-                result["three_years"] = this.three_years;
-
-                return result;
-            }
-        }
-
-        private struct Column
-        {
-            public string name { get; set; }
-            public string type { get; set; }
-            public bool nullable { get; set; }
-            public bool identity { get; set; }
-            public object exemple_value { get; set; }
-            public bool partitioning { get; set; }
-            public bool primary_key { get; set; }
-            public string Default { get; set; }
-            public bool insert_set_to_null { get; set; }
-            public bool read_only { get; set; }
-            public string update_auto { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["name"] = this.name;
-                result["type"] = this.type;
-                result["nullable"] = this.nullable;
-                result["identity"] = this.identity;
-                result["exemple_value"] = this.exemple_value == null? null : this.exemple_value.ToString();
-                result["partitioning"] = this.partitioning;
-                result["primary_key"] = this.primary_key;
-                result["default"] = this.Default;
-                result["insert_set_to_null"] = this.insert_set_to_null;
-                result["read_only"] = this.read_only;
-                result["update_auto"] = this.update_auto;
-
-                return result;
-            }
-        }
-
-        private struct ForeignKey
-        {
-            public string name { get; set; }
-            public string table { get; set; }
-            public List<ForeignKeyColumn> columns { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["name"] = this.name;
-                result["table"] = this.table;
-
-                List<Value> columns = new List<Value>();
-                foreach (var column in this.columns)
-                {
-                    columns.Add(column.Store());
-                }
-                result["columns"] = columns;
-
-                return result;
-            }
-        }
-
-        private struct ForeignKeyColumn
-        {
-            public string local { get; set; }
-            public string foreign { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["local"] = this.local;
-                result["foreign"] = this.foreign;
-
-                return result;
-            }
-        }
-
-        private struct Index
-        {
-            public string name { get; set; }
-            public List<string> columns { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["name"] = this.name;
-
-                List<Value> columns = new List<Value>();
-                foreach (var column in this.columns)
-                {
-                    columns.Add(column);
-                }
-                result["columns"] = columns;
-
-                return result;
-            }
-        }
-
-        private struct ReverseForeignKeys
-        {
-            public string table { get; set; }
-            public List<ForeignKeyColumn> columns { get; set; }
-
-            public Value Store()
-            {
-                Dictionary<Value, Value> result = new Dictionary<Value, Value>();
-                result["table"] = this.table;
-
-                Dictionary<Value, Value> columns = new Dictionary<Value, Value>();
-                foreach (var column in this.columns)
-                {
-                    columns[column.local] = column.Store();
-                }
-                result["columns"] = columns;
-
-                return result;
-            }
-        }
 
         #endregion
     }
